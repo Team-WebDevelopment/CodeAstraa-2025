87a8b96c4da91e10091f8fd05e76c4fc1e91c9b7
KnownBlackHat <knownblackhat@gmail.com>
2025-03-23 21:03:26 -0400

    feat: add timeline page

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Rewards from "./components/Rewards";
 import About from "./pages/About";
 import Landing from "./pages/Landing";
 import ThemePage from "./pages/ThemePage";
+import Timeline from "./pages/Timeline";
 export default function Home() {
     return (
         <>
             <Navbar />
             <Landing />
+            <Timeline />
             <ThemePage />
             <About />
             <Rewards />
